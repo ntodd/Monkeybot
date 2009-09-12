e289253870a5f7c8df83df11c2c40fbac8f6dc0b
Nate Todd <nate@natetodd.com>
2009-09-12 17:34:12 -0400

    fixed constants in cf handler, made trailing space optional on listeners - did not work when trigger word was the last word of a message

--- bot.rb
+++ bot.rb
@@ -232,7 +232,7 @@ room.listen do |message|
   # = General Listeners =
   # =====================
   Listener.all.each do |handler|
-    if message[:message] =~ handler[0]
+    if message[:message] =~ Regexp.new(handler[0])
       room.speak handler[1]
     end
   end
@@ -240,14 +240,14 @@ room.listen do |message|
   # ================
   # = *COLD FUSION =
   # ================
-  TECHNOLOGIES = ["cold fusion", "coldfusion", "CF"]
+  technologies = ["cold fusion", "coldfusion", "CF"]
   insults = ["antiquated", "lame", "needing Chuck Norris to deal the death blow"]
   insults += ["FTS!", "needing not be mentioned anymore"]
   insults += ["..., wait, someone still uses that?", "in my mind stuck in the bucket with COBOL and FORTRAN"]
   insults += ["on it's death bed", "showing it's age", "dead to me"]
   insults += ["amateur", "worse than chapped lips when skiing", "relatively enjoyable compared to a vasectomy"]
   
-  TECHNOLOGIES.each do |t|
+  technologies.each do |t|
     if message[:message].downcase.match(t.downcase)
       room.speak "#{t} is " + insults[rand(insults.size)].to_s
     end
--- lib/listener.rb
+++ lib/listener.rb
@@ -2,9 +2,9 @@ class Listener
   
   # These are the funny listeners.  Segregated so we can turn them off if it gets annoying down the road.
   FUNNY =
-    [/\smom\s/i, "Hey now, no reason to drag someone's mom into this."],
-    [/\sFort Wayne\s/i, "Fort Wayne FTW!"],
-    [/\sgem\s/i, "Oooooh shiny!!!"]
+    [/\smom\s?/i, "Hey now, no reason to drag someone's mom into this."],
+    [/\sFort Wayne\s?/i, "Fort Wayne FTW!"],
+    [/\sgem\s?/i, "Oooooh shiny!!!"]
     
   # Other categories here?
   
