d224735e496f23ae318f82955f2f14de4382340d
Michael Krisher <mike@mikekrisher.com>
2009-10-17 14:04:10 -0400

    status message on pom command

--- bot.rb
+++ bot.rb
@@ -254,7 +254,7 @@ room.listen do |message|
     tstamp = t.strftime("%I:%M%p")
     fstamp = f.strftime("%I:%M%p")
     room.speak "#{user.name} will be away for 25 minutes"
-    user.update_attributes( :status => "doing a pomodoro, be back shortly")
+    user.update_attributes( :status => "started a pomodoro at #{tstamp.to_s} and will be inactive until at least #{fstamp.to_s}")
   end
   
   # ==================
